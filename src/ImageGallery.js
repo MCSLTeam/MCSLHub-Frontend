e6ebd28f9aab35413650d97c03274d51e45961fb
Z_Tsin <66498740+ztsinsun@users.noreply.github.com>
2024-08-31 10:36:25 +0800

    Update ImageGallery.js

--- src/ImageGallery.js
+++ src/ImageGallery.js
@@ -6,7 +6,7 @@ function ImageGallery() {
     const [images, setImages] = useState([]);
 
     useEffect(() => {
-        fetch('http://localhost:8000/fetch-images.php')
+        fetch('/path-to-your-php-script.php')
             .then((response) => response.json())
             .then((data) => {
                 setImages(data.filter(item => item.type === 'file' && item.download_url));
@@ -26,13 +26,69 @@ function ImageGallery() {
             className="my-masonry-grid"
             columnClassName="my-masonry-grid_column">
             {images.map((image) => (
-                <Card key={image.sha} sx={{ borderRadius: 2, marginTop: 8, marginBottom: 4, marginLeft: 2, marginRight: 2 }}>
+                <Card key={image.sha} sx={{ borderRadius: 2, marginBottom: 2 }}>
                     <CardMedia
                         component="img"
-                        height="200"
                         image={image.download_url}
                         alt={image.name}
-                        sx={{ borderRadius: 2 }}
+                        sx={{ 
+                            width: '100%', 
+                            height: 'auto', 
+                            objectFit: 'contain', 
+                            borderRadius: 2 
+                        }}
+                    />
+                    <CardContent>
+                        <Typography variant="h6" component="div">
+                            {image.name}
+                        </Typography>
+                    </CardContent>
+                </Card>
+            ))}
+        </Masonry>
+    );
+}
+
+export default ImageGallery;
+import React, { useEffect, useState } from 'react';
+import Masonry from 'react-masonry-css';
+import { Card, CardContent, CardMedia, Typography } from '@mui/material';
+
+function ImageGallery() {
+    const [images, setImages] = useState([]);
+
+    useEffect(() => {
+        fetch('/path-to-your-php-script.php')
+            .then((response) => response.json())
+            .then((data) => {
+                setImages(data.filter(item => item.type === 'file' && item.download_url));
+            });
+    }, []);
+
+    const breakpointColumnsObj = {
+        default: 4,
+        1100: 3,
+        700: 2,
+        500: 1
+    };
+
+    return (
+        <Masonry
+            breakpointCols={breakpointColumnsObj}
+            className="my-masonry-grid"
+            columnClassName="my-masonry-grid_column">
+            {images.map((image) => (
+                <Card key={image.sha} sx={{ borderRadius: 2, marginBottom: 2 }}>
+                    <CardMedia
+                        component="img"
+                        image={image.download_url}
+                        alt={image.name}
+                        sx={{ 
+                            width: '100%', 
+                            height: 'auto', 
+                            objectFit: 'contain', 
+                            borderRadius: 2 
+                        }}
                     />
                     <CardContent>
                         <Typography variant="h6" component="div">
